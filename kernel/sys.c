7b0bcdad4921a3edb84cd7f28e8025a7793a00ef
Star-Seven <72426083+Star-Seven@users.noreply.github.com>
2024-12-13 11:25:25 +0800

    fs: Introduce SUSFS for KSU.
    832c15a076dd055373bd572ae08ef45ca33b0550

--- kernel/sys.c
+++ kernel/sys.c
@@ -73,6 +73,13 @@
 #include <asm/io.h>
 #include <asm/unistd.h>
 
+#ifdef CONFIG_KSU_SUSFS
+#include <linux/susfs.h>
+#endif
+
+#ifdef CONFIG_SECURITY_DEFEX
+#include <linux/defex.h>
+#endif
 #ifndef SET_UNALIGN_CTL
 # define SET_UNALIGN_CTL(a, b)	(-EINVAL)
 #endif
@@ -1217,6 +1224,9 @@ SYSCALL_DEFINE1(uname, struct old_utsname __user *, name)
 	down_read(&uts_sem);
 	memcpy(&tmp, utsname(), sizeof(tmp));
 	up_read(&uts_sem);
+#ifdef CONFIG_KSU_SUSFS_SPOOF_UNAME
+	susfs_spoof_uname(&tmp);
+#endif
 	if (copy_to_user(name, &tmp, sizeof(tmp)))
 		return -EFAULT;
 
